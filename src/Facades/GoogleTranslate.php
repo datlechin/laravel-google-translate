3cd7a85fecb26bc2b831b331563add1f2054ffe9
Ngo Quoc Dat <datlechin@gmail.com>
2023-05-30 18:01:50 +0700

    Update docblock for facade

--- src/Facades/GoogleTranslate.php
+++ src/Facades/GoogleTranslate.php
@@ -4,6 +4,13 @@ namespace Datlechin\GoogleTranslate\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Datlechin\GoogleTranslate\GoogleTranslate withSource(string $source)
+ * @method static \Datlechin\GoogleTranslate\GoogleTranslate withTarget(string $target)
+ * @method static \Datlechin\GoogleTranslate\GoogleTranslateResult translate(string $text, string|null $source = null, string|null $target = null)
+ *
+ * @see \Datlechin\GoogleTranslate\GoogleTranslate
+ */
 class GoogleTranslate extends Facade
 {
     protected static function getFacadeAccessor(): string
